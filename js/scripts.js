75599d15e7c9a60d1e17ab04ff2ab80fc17142e4
Neil Larion <neil.larion@gmail.com>
2016-01-18 11:32:15 -0800

    Add logic and test to return true when years are divisible by 400

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 var leapYear = function(year){
-  if((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0)) {
+    return true;
+  } else if (year % 400 ===0) {
     return true;
   } else {
     return false;
